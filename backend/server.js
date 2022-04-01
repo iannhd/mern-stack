bb82423cd55505c47ec88a1fc96a6fd97a47de16
iannhd <in.hadyan@gmail.com>
2022-04-01 21:16:09 +0700

    Initial API for goals

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.PORT || 5000
+const { errorHandler } = require('./middleware/errorMiddleware')
+const colors = require('colors')
+const connectDB = require('./config/db')
+const app = express()
+
+connectDB()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => {
+    console.log(`server on ${port}`)
+})
